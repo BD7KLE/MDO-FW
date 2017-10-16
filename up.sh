9ed681ca4fd4d5959c3d26087990c6230e69a1d4
BD7KLE <30582352+BD7KLE@users.noreply.github.com>
2017-10-16 12:32:03 +0800

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #Install Required Software
-git clone https://github.com/candrist/mmdvm-image.git /home/pi/Scripts
+git clone https://github.com/BD7KLE/MDO-FW/raw/master/UHF%203.0.hex /tmp
 
 # DV-Mega Mods
 sudo systemctl stop mmdvmhost.service
 sudo mount -o remount,rw /
-stm32flash -v -w UHF.hex -g 0x0 -R -i 23,-22,22:-23,22 /dev/ttyAMA0 
+stm32flash -v -w /tmp/UHF%203.0.hex -g 0x0 -R -i 23,-22,22:-23,22 /dev/ttyAMA0 
 sudo mount -o remount,ro /
 sudo systemctl restart mmdvmhost.service
 
