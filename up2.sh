81b269e8c7ae1b9bbcc5b252c8225b893db1e0f4
BD7KLE <30582352+BD7KLE@users.noreply.github.com>
2018-02-25 22:13:07 +0800

    Update and rename upV.sh to up2.sh

--- up2.sh
+++ up2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 sudo mount -o remount,rw /
 #Install Required Software
-wget https://github.com/BD7KLE/MDO-FW/raw/master/VHF-3.0.hex
+wget https://github.com/BD7KLE/MDO-FW/raw/master/UHF302018-2-25.hex
 # DV-Mega Mods
 sudo systemctl stop mmdvmhost.timer
 sudo systemctl stop mmdvmhost.service
-sudo stm32flash -v -w VHF-3.0.hex -g 0x0 -R -i 23,-22,22:-23,22 /dev/ttyAMA0 
+sudo stm32flash -v -w UHF302018-2-25.hex -g 0x0 -R -i 23,-22,22:-23,22 /dev/ttyAMA0 
 sudo systemctl start mmdvmhost.timer
 sudo systemctl restart mmdvmhost.service
-sudo rm VHF-3.0.hex
-sudo rm upV.sh
+sudo rm UHF302018-2-25.hex
+sudo rm up2.sh
 sudo mount -o remount,ro /
