34d9cc874920c78c6342c6cfc0147504bc0b252b
BD7KLE <30582352+BD7KLE@users.noreply.github.com>
2018-02-26 18:39:53 +0800

    Create vhf.sh

---
+++ vhf.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo mount -o remount,rw /
+#Install Required Software
+wget https://github.com/BD7KLE/MDO-FW/raw/master/VHF2018-2-26.hex
+# DV-Mega Mods
+sudo systemctl stop mmdvmhost.timer
+sudo systemctl stop mmdvmhost.service
+sudo stm32flash -v -w VHF2018-2-26.hex -g 0x0 -R -i 23,-22,22:-23,22 /dev/ttyAMA0 
+sudo systemctl start mmdvmhost.timer
+sudo systemctl restart mmdvmhost.service
+sudo rm VHF2018-2-26.hex
+sudo rm up.sh
+sudo mount -o remount,ro /
