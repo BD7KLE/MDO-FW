d2a883bd576fbd9feddfc247c5e83fa5f45d2df9
BD7KLE <30582352+BD7KLE@users.noreply.github.com>
2018-06-21 22:15:35 +0800

    Update up2.sh

--- up2.sh
+++ up2.sh
@@ -5,9 +5,9 @@ wget https://github.com/BD7KLE/MDO-FW/raw/master/UHF302018-6-21.hex
 # DV-Mega Mods
 sudo systemctl stop mmdvmhost.timer
 sudo systemctl stop mmdvmhost.service
-sudo stm32flash -v -w UHF302018-2-26.hex -g 0x0 -R -i 23,-22,22:-23,22 /dev/ttyAMA0 
+sudo stm32flash -v -w UHF302018-6-21.hex -g 0x0 -R -i 23,-22,22:-23,22 /dev/ttyAMA0 
 sudo systemctl start mmdvmhost.timer
 sudo systemctl restart mmdvmhost.service
-sudo rm UHF302018-2-26.hex
+sudo rm UHF302018-6-21.hex
 sudo rm up2.sh
 sudo mount -o remount,ro /
